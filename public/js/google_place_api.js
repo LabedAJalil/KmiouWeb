d4add55721c2c3bf91b83319e8ebe66637f24754
unknown <Younes>
2022-09-09 18:20:29 +0100

    first change

--- public/js/google_place_api.js
+++
@@ -1,4 +0,0 @@
-
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages : 'ar,fr,en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
-}
